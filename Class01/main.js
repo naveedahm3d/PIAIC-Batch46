2716546579b46a873de3ceedfc1d7e826c47c54c
naveedahm3d <48651347+naveedahm3d@users.noreply.github.com>
2023-07-06 18:21:26 +0500

    Add files via upload

--- Class01/main.js
+++ Class01/main.js
@@ -1,2 +1,18 @@
-var namethis = ("This is testing only");
-console.log(namethis);
+// 2.	Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personName1 = ("Hello Eric, would you like to learn some Python today?");
+console.log(personName1);
+//3.	Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var personName = "John Sander";
+console.log("Lowercase: " + personName.toLowerCase());
+console.log("Uppercase: " + personName.toUpperCase());
+// Titlecase implementation
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+console.log("Titlecase: " + toTitleCase(personName));
+//4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+var quote = 'A person who never made a mistake never tried anything new.';
+var author = 'Albert Einstein';
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
--- Class01/main.ts
+++ Class01/main.ts
@@ -1,2 +1,26 @@
-let namethis = ("This is testing only");
-console.log (namethis);
+// 2.	Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let personName1 = ("Hello Eric, would you like to learn some Python today?");
+console.log (personName1);
+
+//3.	Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName: string = "John Sander";
+
+console.log("Lowercase: " + personName.toLowerCase());
+console.log("Uppercase: " + personName.toUpperCase());
+
+// Titlecase implementation
+function toTitleCase(str: string): string {
+  return str.replace(/\w\S*/g, (txt: string) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+console.log("Titlecase: " + toTitleCase(personName));
+
+
+//4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+const quote: string = 'A person who never made a mistake never tried anything new.';
+const author: string = 'Albert Einstein';
+
+console.log(`${author} once said, "${quote}"`);
+
